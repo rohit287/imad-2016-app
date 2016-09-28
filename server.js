dcdb7fc3736b1f04f217d6a51e07788f7d4bba76
rohit287 <rohitmittal287@gmail.com>
2016-09-28 23:11:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,8 @@ app.get('/', function (req, res) {
 app.get('/articleName',function (req, res) 
 //articleName == article-one
 //articles[articleName] == {} content for article one
-var articleName = req.parans.articleName;
+{
+    var articleName = req.parans.articleName;
  res.send(createTemplate(articles[articleName]));
 });
 app.get('/article-two',function (req, res) 
