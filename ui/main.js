40abe2185919660c4d6aa90fbd5f6bfed2b953d6
rohit287 <rohitmittal287@gmail.com>
2016-10-10 23:46:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,15 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New value';
-// move the image
+//move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight ()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function()
 {
-     img.style.marginLeft = '100px';
+     var interval = setInterval(moveRight,100);
 };
